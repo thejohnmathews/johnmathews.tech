908d63a1d93b1c3471f084d32da8f30a488bf1e7
mathew7 <mathew7@clemsonl.edu>
2024-05-02 15:59:48 -0400

    Hello World, organizing local files

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # johnmathews.tech
 
-My personal website acting as a resume and a blog of my travels!
+I knew that when I graduated from Clemson University, I wanted to travel and experience other cultures. I knew that in doing so, I would need to keep my computer science/programming skills alive. So I bought this domain! 
+
+This website will be made from scratch (with the help of frameworks, of course) showcasing my personal résumé as well as a "blog" section so that my family can see what I am doing overseas. 
 
 ## Technologies used: 
+    1) ReactJS (18.3.1)
+    2) NextUI (2.3.6)
--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import * as React from "react";
-import logo from './logo.svg';
-import './App.css';
+// johnmathews.tech - App.js
+// Entry point into the website
 
-// 1. import `NextUIProvider` component
+/*
+TODO:
+  - Create home screen
+  -> it should behave as a about me page with links?
+  -> menu option to resume
+  -> menu option to personal blog
+
+Thoughts:
+  - I destroyed the initial public/index.html, did I need it?
+  - 
+*/
+
+
+// import statements
+import * as React from "react";
+import './App.css';
 import {NextUIProvider} from "@nextui-org/react";
 
 function App() {
