e6908bb9838eb5dd8e81ada91a149d4345ce125e
thejohnmathews <thejohnmathews@gmail.com>
2024-12-10 14:32:01 +0100

    adding social links

--- index.php
+++ index.php
@@ -39,8 +39,20 @@
         </main>
 
         <!-- contents of footer -->
-        <footer id = "fixed-footer">
+        <footer id="fixed-footer">
             <p>&copy; 2024 John Mathews</p>
+            <div id="social-media-links">
+                <a href="https://www.linkedin.com/in/thejohnmathews" target="_blank" title="LinkedIn">
+                    <img src="assets/icons/linkedin.png" alt="LinkedIn Logo" class="social-icon">
+                </a>
+                <a href="https://github.com/thejohnmathews" target="_blank" title="GitHub">
+                    <img src="assets/icons/github.png" alt="GitHub Logo" class="social-icon">
+                </a>
+                <a href="https://www.instagram.com/john.em" target="_blank" title="Instagram">
+                    <img src="assets/icons/instagram.png" alt="Instagram Logo" class="social-icon">
+                </a>
+            </div>
         </footer>
+
     </body>
 </html>
